08d7c2ed2424264905f434759698f6c3ef204e91
=mohamed Hamed <mh674281@gmail.com>
2024-02-17 00:41:52 +0200

    Learn OOP with JAVASCRIPT

--- app.js
+++ app.js
@@ -1,5 +1,50 @@
-const user = {
+// const user = {
+//   name: "Mohamed",
+//   gender: "male",
+//   age: 23,
+//   calcAge: (age) => {
+//     const ageinDAys = age * 365;
+//     return ageinDAys;
+//   },
+// };
+
+// console.log(user.age);
+// console.log(user.calcAge(user.age));
+// ========================================
+
+// let method = ["body", "params", "query", "file"];
+// let RESTApis = {
+//   body: () => `hellow`,
+//   params: "ahmed params",
+//   query: "ahmed query",
+//   file: "fk me harder",
+// };
+
+// for (const key of method) {
+// // console.log(RESTApis[key]());
+// console.log(RESTApis[method[0]]());
+// }
+
+// let user = new Object();
+let user = {
   name: "Mohamed",
-  gender: "male",
+  params: "ahmed params",
+  query: "ahmed query",
+  file: "fk me harder",
+  getName: function () {
+    console.log(`you name is ${user.naem}`);
+  },
 };
 
+// let newuser = Object.create(user);
+// newuser.name = "ahmed";
+// console.log(newuser.getName());
+
+let mo = {};
+Object.defineProperty(mo, "name", { value: "Ahmed", configurable: false });
+
+console.log(mo);
+
+for (const key in user) {
+  console.log(user[key]);
+}
